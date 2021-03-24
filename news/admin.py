8b59f02adf8e62c85ffdb98a955dea659d06ef30
ibrahimciftci <ibrhm.cftci@hotmail.com>
2021-03-24 17:17:01 +0300

    3. commit

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import News
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+#    list_filter = ['status']
+
+
+
+admin.site.register(News,NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 
-class category(models.Model):
+
+class News(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=200)
-    keywords = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    image = models.ImageField(blank=True,upload_to='images/')
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-

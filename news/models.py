892d069fa1ffde847bc21ffe8a1f24b1cd401b92
ibrahimciftci <ibrhm.cftci@hotmail.com>
2021-03-24 18:39:32 +0300

    4. commit

--- news/models.py
+++ news/models.py
@@ -15,3 +15,77 @@ class News(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+class Menu(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+class Content(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    news =models.ForeignKey(News, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
+
+
+
+
+'''class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayir'),
+    )
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(blank=True,max_length=255)
+    company = models.CharField(blank=True,max_length=150)
+    address = models.CharField(blank=True,max_length=150)
+    phone = models.CharField(blank=True,max_length=15)
+    fax = models.CharField(blank=True,max_length=15)
+    email = models.CharField(blank=True,max_length=50)
+    smtpserver = models.CharField(blank=True,max_length=20)
+    smtpemail = models.CharField(blank=True,max_length=20)
+    smtppassword = models.CharField(blank=True,max_length=10)
+    smtpport = models.CharField(blank=True,max_length=5)
+    icon = models.ImageField(blank=True,upload_to='images/')
+    facebook = models.CharField(blank=True,max_length=50)
+    aboutus = RichTextUploadingField(blank=True)
+    contact = RichTextUploadingField(blank=True)
+    referances = RichTextUploadingField(blank=True)
+    status = models.CharField(max_length=10,choices=STATUS)
+    create_at = models.DateTimeField (auto_now_add=True)
+    update_at = models.DateTimeField (auto_now=True)
+
+
+
+    def __str__(self):
+        return self.title
+
+'''
+

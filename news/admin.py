94b0237193fe62487292195aa6dc8c5c3f5a72d7
ibrahimciftci <ibrhm.cftci@hotmail.com>
2021-03-26 15:53:26 +0300

    7. commit

--- news/admin.py
+++ news/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 
-from news.models import News, Menu, Content
+from news.models import News, Menu, Content, Images
+
+class ContentImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
-
-
-#    list_filter = ['status']
+#   list_filter = ['status']
 
 class ContentAdmin(admin.ModelAdmin):
     list_display = ['title', 'detail','news']
     list_filter = ['news']
+    inlines = [ContentImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'image']
+
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Menu)
 admin.site.register(Content,ContentAdmin)
+admin.site.register(Images, ImagesAdmin)
--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -15,15 +15,17 @@ class News(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
+
 
 class Menu(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -32,16 +34,18 @@ class Menu(models.Model):
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
+
 
 class Content(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    news =models.ForeignKey(News, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -49,43 +53,16 @@ class Content(models.Model):
     status = models.CharField(max_length=10, choices=STATUS)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.title
-
-
-
-
-
-'''class Setting(models.Model):
-    STATUS = (
-        ('True', 'Evet'),
-        ('False', 'Hayir'),
-    )
-    title = models.CharField(max_length=150)
-    keywords = models.CharField(max_length=255)
-    description = models.CharField(blank=True,max_length=255)
-    company = models.CharField(blank=True,max_length=150)
-    address = models.CharField(blank=True,max_length=150)
-    phone = models.CharField(blank=True,max_length=15)
-    fax = models.CharField(blank=True,max_length=15)
-    email = models.CharField(blank=True,max_length=50)
-    smtpserver = models.CharField(blank=True,max_length=20)
-    smtpemail = models.CharField(blank=True,max_length=20)
-    smtppassword = models.CharField(blank=True,max_length=10)
-    smtpport = models.CharField(blank=True,max_length=5)
-    icon = models.ImageField(blank=True,upload_to='images/')
-    facebook = models.CharField(blank=True,max_length=50)
-    aboutus = RichTextUploadingField(blank=True)
-    contact = RichTextUploadingField(blank=True)
-    referances = RichTextUploadingField(blank=True)
-    status = models.CharField(max_length=10,choices=STATUS)
-    create_at = models.DateTimeField (auto_now_add=True)
-    update_at = models.DateTimeField (auto_now=True)
-
-
 
     def __str__(self):
         return self.title
 
-'''
+
+class Images(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
 

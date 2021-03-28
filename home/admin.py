a4f954ff5f91eb382f8492d80d7cbd3fed19505e
ibrahimciftci <ibrhm.cftci@hotmail.com>
2021-03-28 18:20:36 +0300

    12. commit

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from home.models import Setting
+from home.models import Setting, ContactFormMessage, ContactFormu
 
+
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject', 'message', 'note',  'status']
+    list_filter = ['status']
+
+
+admin.site.register(ContactFormMessage, ContactFormMessageAdmin)
 admin.site.register(Setting)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
-
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from home.models import Setting
+from home.models import Setting, ContactFormMessage, ContactFormu
 
 
 def index(request):
@@ -23,3 +24,22 @@ def iletisim(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting' : setting, 'page' : 'iletisim'}
     return render(request, 'iletisim.html', context)
+
+def iletisim(request):
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajiniz basariyla gonderildi.")
+            return HttpResponseRedirect('/iletisim')
+    setting = Setting.objects.get(pk=1)
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
+    return render(request, 'iletisim.html', context)
+
